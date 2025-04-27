e39d17c49883428324c5eb667af3941e46104420
uki <wijekoonuthpala123@gmail.com>
2025-04-27 14:19:16 +0530

    icon updte

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
+// filepath: d:\Work\next.js\twiiter [layout.tsx](http://_vscodecontentref_/3)
 import LeftBar from "@/components/LeftBar";
 import "./globals.css";
 import RightBar from "@/components/RightBar";
+
+export const metadata = {
+  title: "BLUR TEXT", // Set your desired title here
+  description: "This is a description for SEO purposes.", // Optional
+};
 
 export default function RootLayout({
   children,
@@ -9,8 +15,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.png" />
+        {/* Add the favicon */}
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
       <body>
-        <div className=" max-w-screen-md lg:max-w-screen-lg mx-auto flex justify-between">
+        <div className="max-w-screen-md lg:max-w-screen-lg mx-auto flex justify-between">
           <div className="">
             <LeftBar />
           </div>
